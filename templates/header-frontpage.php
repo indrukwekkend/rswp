db2ff1d8003a80dda9970ca383f8c72cdd1ee0a3
odil-io <github@odil.io>
2017-11-09 17:42:59 +0100

    Basis voor jumbotron toegevoegd

--- templates/header-frontpage.php
+++ templates/header-frontpage.php
@@ -1,5 +1,3 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="header-frontpage">
-  <h1><?= Titles\title(); ?></h1>
-</div>
+<?php get_template_part('templates/sections/jumbotron'); ?>
---
+++ templates/sections/jumbotron.php
@@ -0,0 +1,9 @@
+<section class="jumbotron text-center">
+	<div class="container">
+		<h1 class="jumbotron-heading">Jumbotron titel</h1>
+		<p class="lead">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
+		<p>
+			<a href="#cta-1" class="btn btn-primary">CTA #1</a> <a href="#cta-2" class="btn btn-secondary">CTA #2</a>
+		</p>
+	</div>
+</section>
