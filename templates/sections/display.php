a9aaeb39ff4b458a70abdb8beed61b66ca6b0f14
odil-io <github@odil.io>
2017-11-10 11:55:51 +0100

    Display sectie toegevoegd
    Geeft de gebruiker keuze uit alle 4 de Display headers van bootstrap.

---
+++ templates/sections/display.php
@@ -0,0 +1,12 @@
+<section class="container">
+	<div class="row text-center">
+		<div class="col-12">
+			<h1 class="<?php the_sub_field('display_type'); ?>">
+				<?php the_sub_field('title'); ?>
+			</h1>
+			<p class="lead">
+				<?php the_sub_field('lead'); ?>
+			</p>
+		</div>
+	</div>
+</section>
