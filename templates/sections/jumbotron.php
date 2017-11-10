29ab4d459606bb63b62e609e213794ed70db3a0c
odil-io <github@odil.io>
2017-11-10 11:08:21 +0100

    Opschonen

--- templates/sections/jumbotron.php
+++ templates/sections/jumbotron.php
@@ -1,9 +1,11 @@
-<section class="jumbotron text-center">
+<section class="jumbotron text-center m-0">
 	<div class="container">
-		<h1 class="jumbotron-heading">Jumbotron titel</h1>
-		<p class="lead">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
-		<p>
-			<a href="#cta-1" class="btn btn-primary">CTA #1</a> <a href="#cta-2" class="btn btn-secondary">CTA #2</a>
-		</p>
+		<div class="row">
+			<h1 class="jumbotron-heading">Jumbotron titel</h1>
+			<p class="lead">Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
+			<p>
+				<a href="#cta-1" class="btn btn-primary">CTA #1</a> <a href="#cta-2" class="btn btn-secondary">CTA #2</a>
+			</p>
+		</div>
 	</div>
 </section>
