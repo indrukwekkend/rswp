ce2b7c0b1b8eac28bb632d4f74fe6ec32f3b41fc
odil-io <github@odil.io>
2017-11-10 12:10:43 +0100

    Featured sectie toegevoegd

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,9 +1,13 @@
 <?php if( have_rows('sections') ) : ?>
 	<?php while( have_rows('sections') ) : the_row(); ?>
-	
-	<?php if( get_row_layout() == 'section_display' ): ?>
-		<?php get_template_part('templates/sections/display'); ?>
-	<?php endif;?>
-	
+
+		<?php if( get_row_layout() == 'section_display' ): ?>
+			<?php get_template_part('templates/sections/display'); ?>
+		<?php endif;?>
+
+		<?php if( get_row_layout() == 'section_featured' ): ?>
+			<?php get_template_part('templates/sections/featured'); ?>
+		<?php endif;?>
+
 	<?php endwhile;?>
 <?php endif;?>
