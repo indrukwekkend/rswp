da48b876c0a6f801129c892f3eb349813f94e520
odil-io <github@odil.io>
2017-11-17 13:46:10 +0100

    Kleine wijzigingen
    Container hierarchie in header gewijzigd
    Contianer toegevoegd aan index

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
-<?php get_template_part('templates/header', 'page'); ?>
+<section class="index">
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning" role="warning">
-    <?php _e('Helaas, er zijn (nog) geen berichten om te lezen :(', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
+	<?php get_template_part('templates/header', 'page'); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
+	<?php if (!have_posts()) : ?>
 
-<?php the_posts_navigation(); ?>
+		<div class="alert alert-warning" role="warning">
+
+			<?php _e('Helaas, er zijn (nog) geen berichten om te lezen :(', 'sage'); ?>
+
+		</div>
+
+		<?php get_search_form(); ?>
+
+	<?php endif; ?>
+
+	<?php while (have_posts()) : the_post(); ?>
+
+		<div class="container">
+
+			<?php get_template_part('templates/content', get_post_type() ? get_post_type() : get_post_format()); ?>
+
+		</div>
+
+	<?php endwhile; ?>
+
+	<?php the_posts_navigation(); ?>
+
+</section>
--- templates/header-page.php
+++ templates/header-page.php
@@ -1,7 +1,7 @@
 <?php use Roots\Sage\Titles; ?>
 
-<section class="container">
-    <div class="header-page">
+<section class="header-page">
+    <div class="container">
       <h1><?= Titles\title(); ?></h1>
     </div>
 </section>
