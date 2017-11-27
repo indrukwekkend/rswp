aefa6fe18fb190aef81244f31dfa5d364c82b296
odilio <github@odil.io>
2017-11-27 11:59:00 +0100

    Posts sectie opgeschoont
    Excerpt verwijderd
    Opmaak gewijzigd

--- templates/sections/posts.php
+++ templates/sections/posts.php
@@ -3,75 +3,83 @@
 $lead = get_sub_field('lead');
 $taxonomy = get_sub_field('taxonomy');
 $aantal = get_sub_field('amount');
+
+$args = array(
+  'posts_per_page' =>  $aantal,
+  'tax_query' => array(
+    'relation' => 'AND',
+      array(
+        'taxonomy' => 'category',
+        'field' => 'id',
+        'terms' => array( $taxonomy )
+      )
+    )
+  );
+
+$the_query = new WP_Query( $args );
 ?>
 
 
 <section class="posts">
 
-	<div class="row">
+  <div class="row">
 
-		<div class="col-12 py-4 text-center">
+    <div class="col-12 py-4 text-center">
 
-			<h1 class="display-4"><?php the_sub_field('title'); ?></h1>
+      <h1 class="display-4">
+        <?php the_sub_field('title'); ?>
+      </h1>
 
-			<p class="lead"><?php the_sub_field('lead'); ?></p>
+      <p class="lead">
+        <?php the_sub_field('lead'); ?>
+      </p>
 
-		</div>
+    </div>
 
-	</div>
+  </div>
 
-	<div class="row">
+  <div class="row">
 
-		<?php
+    <?php if ( $the_query->have_posts() ): ?>
 
-		$args = array(
-			'posts_per_page' =>  $aantal,
-			'tax_query' => array(
-			'relation' => 'AND',
-			array(
-				'taxonomy' => 'category',
-				'field' => 'id',
-				'terms' => array( $taxonomy )
-			)
-			)
-		);
+      <?php while ( $the_query->have_posts() ): ?>
 
-		$the_query = new WP_Query( $args );
+        <?php $the_query->the_post(); ?>
 
-		?>
+          <div class="card-deck col-12 col-md-6 col-lg">
 
-			<?php if ( $the_query->have_posts() ): ?>
+            <div class="card mb-4">
 
-				<?php while ( $the_query->have_posts() ): ?>
+              <div class="card-body">
 
-				<?php $the_query->the_post(); ?>
+                <h4 class="card-title">
+                  <?php the_title(); ?>
+                </h4>
 
-					<div class="col-12 col-md-6 col-lg">
+                <p class="card-text text-muted">
+                  <small><?= get_the_date( get_option( 'date_format' ) );?></small>
+                </p>
 
-						<div class="card mb-4">
+                <div class="card-text">
+                  <?php the_excerpt(); ?>
+                </div>
 
-							<div class="card-body">
+              </div>
 
-								<h4 class="card-title"><?php the_title(); ?></h4>
+              <div class="card-footer">
+                <a href="<?= get_post_permalink(); ?>" class="card-link">Lees verder</a>
+              </div>
 
-								<h6 class="card-subtitle mb-2 text-muted"><?= get_the_date( get_option( 'date_format' ) );?></h6>
+            </div>
 
-								<div class="card-text"><?php the_excerpt(); ?></div>
+          </div>
 
-								<a href="<?= get_post_permalink(); ?>" class="card-link">Lees verder</a>
+        <?php endwhile;?>
 
-							</div>
+      <?php wp_reset_postdata(); ?>
 
-						</div>
+    <?php endif;?>
 
-					</div>
-
-				<?php endwhile;?>
-
-			<?php wp_reset_postdata(); ?>
-
-		<?php endif;?>
-
-	</div>
+  </div>
 
 </section>
