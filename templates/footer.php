b7898d005a227a114b75bddf1c5adb8e83fb9e6a
odil-io <github@odil.io>
2017-12-11 12:24:47 +0100

    Footer styling

--- templates/footer.php
+++ templates/footer.php
@@ -5,8 +5,8 @@
 	</div>
 
 	<div class="row bg-secondary">
-		<div class="col-12">
-			<nav class="navbar-light navbar-expand">
+		<div class="col-12 py-3">
+			<nav class="navbar-dark navbar-expand">
 				<?php
                 wp_nav_menu(array(
                     'menu' => 'primary_navigation',
@@ -25,7 +25,7 @@
 
 	<div class="row">
 		<div class="col-12 text-center">
-			<p class="text-muted">
+			<p class="text-muted my-2">
 				<small>
 					<i class="fa fa-phone text-secondary mr-1"></i><a class="text-muted" href="tel:0299820130" title="Telefoonnummer van RSWP">0299 82 01 30</a>
 					<i class="fa fa-envelope text-secondary ml-2 mr-1"></i><a class="text-muted" href="mailto:rswp.nl">info@rswp.nl</a>
