a7a74d55b6ae6fdcd87bd27b106f11c02b07581a
odilio <github@odil.io>
2017-12-14 14:05:04 +0100

    Menu labels toegevoegd

--- lib/posts.php
+++ lib/posts.php
@@ -13,7 +13,13 @@ function register_cuztom_post_types() {
 	// Activiteiten
 	$labels = [
 		'name' => _x( 'Activiteiten', 'General Name', 'sage' ),
-		'singular_name' => _x( 'Activiteiten' , 'Singular Name', 'sage' ),
+		'singular_name' => _x( 'Activiteit' , 'Singular Name', 'sage' ),
+		'add_new' => _x('Nieuwe activiteit', 'sage'),
+		'add_new_item' => _x('Activiteit toevoegen', 'sage'),
+		'edit_item' => _x('Activiteit aanpassen', 'sage'),
+		'new_item' => _x('Nieuwe activiteit toevoegen', 'sage'),
+		'view_item' => _x('Bekijk activiteit', 'sage'),
+		'view_items' => _x('Bekijk activiteiten', 'sage'),
 	];
 
 	$args = [
@@ -30,7 +36,13 @@ function register_cuztom_post_types() {
 	// Vacatures
 	$labels = [
 		'name' => _x( 'Vacatures', 'General Name', 'sage' ),
-		'singular_name' => _x( 'Vacatures' , 'Singular Name', 'sage' ),
+		'singular_name' => _x( 'Vacature' , 'Singular Name', 'sage' ),
+		'add_new' => _x('Nieuwe vacature', 'sage'),
+		'add_new_item' => _x('Vacature toevoegen', 'sage'),
+		'edit_item' => _x('Vacature aanpassen', 'sage'),
+		'new_item' => _x('Nieuwe vacature toevoegen', 'sage'),
+		'view_item' => _x('Bekijk vacature', 'sage'),
+		'view_items' => _x('Bekijk vacatures', 'sage'),
 	];
 
 	$args = [
@@ -47,7 +59,13 @@ function register_cuztom_post_types() {
 	// Locaties
 	$labels = [
 		'name' => _x( 'Locaties', 'General Name', 'sage' ),
-		'singular_name' => _x( 'Locaties' , 'Singular Name', 'sage' ),
+		'singular_name' => _x( 'Locatie' , 'Singular Name', 'sage' ),
+		'add_new' => _x('Nieuwe locatie', 'sage'),
+		'add_new_item' => _x('Locatie toevoegen', 'sage'),
+		'edit_item' => _x('Locatie aanpassen', 'sage'),
+		'new_item' => _x('Nieuwe locatie Toevoegen', 'sage'),
+		'view_item' => _x('Bekijk locatie', 'sage'),
+		'view_items' => _x('Bekijk locaties', 'sage'),
 	];
 
 	$args = [
@@ -65,7 +83,13 @@ function register_cuztom_post_types() {
 	// Diensten
 	$labels = [
 		'name' => _x( 'Diensten', 'General Name', 'sage' ),
-		'singular_name' => _x( 'Diensten' , 'Singular Name', 'sage' ),
+		'singular_name' => _x( 'Dienst' , 'Singular Name', 'sage' ),
+		'add_new' => _x('Nieuwe dienst', 'sage'),
+		'add_new_item' => _x('Dienst toevoegen', 'sage'),
+		'edit_item' => _x('Dienst aanpassen', 'sage'),
+		'new_item' => _x('Nieuwe dienst toevoegen', 'sage'),
+		'view_item' => _x('Bekijk dienst', 'sage'),
+		'view_items' => _x('Bekijk diensten', 'sage'),
 	];
 
 	$args = [
