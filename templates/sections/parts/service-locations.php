19c6052fb49c5a08ad3182e7ac9238111dd211a1
odilio <github@odil.io>
2017-12-15 15:19:24 +0100

    'Aangeboden in'-lijst geupdate

---
+++ templates/sections/parts/service-locations.php
@@ -0,0 +1,33 @@
+<div class="service-locations">
+
+	<?php $post_objects = get_field('location_service'); ?>
+
+	<?php if($post_objects): ?>
+
+		<?php $total_tags = count($post_objects); ?>
+
+		<?php if($post_objects && ($total_tags > 1) ): ?>
+			<?php $i=0; ?>
+
+			<div><b>Aangeboden in:</b></div>
+
+			<div class="locations">
+				<?php foreach($post_objects as $post): ?>
+					<?php setup_postdata($post); ?>
+					<?php $i++; ?>
+					<?php
+					if($i < ($total_tags - 1) ):
+						$punctuation = ', ';
+					elseif($i < ($total_tags) ):
+						$punctuation = " & ";
+					else:
+						$punctuation = "";
+					endif;
+					?>
+					<a class="text-primary" href="<?= get_the_permalink($location->ID); ?>"><b><?= get_the_title($location->ID); ?></b></a><?= $punctuation; ?>
+				<?php endforeach; ?>
+			</div>
+		<?php endif;?>
+		<?php wp_reset_postdata(); ?>
+	<?php endif;?>
+</div>
