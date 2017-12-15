7b95a9617d6b9ea77d336e010352064a619a5a99
odilio <github@odil.io>
2017-12-15 15:18:31 +0100

    Fix: Merriweather 300i vervangen met 700i

--- templates/content-single-service.php
+++ templates/content-single-service.php
@@ -1,16 +1,44 @@
 <?php while (have_posts()) : the_post(); ?>
 	<section class="location-single">
 		<div class="row mt-5 mb-4">
+
 			<div class="col-5">
 				<div><? the_content(); ?></div>
 				<div class="service-locations">
-					<?php $locations = get_field('location_service'); ?>
-					<?php foreach($locations as $location): ?>
-						<?= get_the_title($location->ID); ?>
 
-					<?php endforeach; ?>
+					<?php $post_objects = get_field('location_service'); ?>
+
+					<?php if($post_objects): ?>
+
+						<?php $total_tags = count($post_objects); ?>
+
+						<?php if($post_objects && ($total_tags > 1) ): ?>
+							<?php $i=0; ?>
+
+							<div><b>Aangeboden in:</b></div>
+
+							<div class="locations">
+								<?php foreach($post_objects as $post): ?>
+									<?php setup_postdata($post); ?>
+									<?php $i++; ?>
+									<?php
+									if($i < ($total_tags - 1) ):
+										$punctuation = ', ';
+									elseif($i < ($total_tags) ):
+										$punctuation = " & ";
+									else:
+										$punctuation = "";
+									endif;
+									?>
+									<a class="text-primary" href="<?= get_the_permalink($location->ID); ?>"><b><?= get_the_title($location->ID); ?></b></a><?= $punctuation; ?>
+								<?php endforeach; ?>
+							</div>
+						<?php endif;?>
+						<?php wp_reset_postdata(); ?>
+					<?php endif;?>
 				</div>
 			</div>
+
 			<div class="col-3">
 				<?php $image = get_field('image'); ?>
 
@@ -26,9 +54,11 @@
 
 				<?php endif; ?>
 			</div>
+
 			<div class="col-4">
 				<?php get_template_part('templates/sections/parts/locations','list'); ?>
 			</div>
 		</div>
+
 	</section>
 <?php endwhile; ?>
