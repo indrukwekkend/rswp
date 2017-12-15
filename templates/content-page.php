0edbdb8b0954313d47e9837f5b6051316b4adb3d
odilio <github@odil.io>
2017-12-15 11:31:13 +0100

    page template geupdate
    Deze toont nu standeerd de locatie lijst.

--- templates/content-page.php
+++ templates/content-page.php
@@ -3,11 +3,11 @@
 
 		<div class="col-6 pt-5">
 			<?php get_template_part('templates/header','page'); ?>
-			<? the_content(); ?>
+			<?php the_content(); ?>
 		</div>
 
 		<div class="col-6 px-4">
-
+			<?php get_template_part('templates/sections/parts/locations','list'); ?>
 		</div>
 
 	</div>
