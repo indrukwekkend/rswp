e7378bc30d6e12114def313f0392cd93eb185beb
odilio <github@odil.io>
2017-12-18 10:36:46 +0100

    Fix: Margins/padding

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,12 +1,12 @@
 <section class="content">
-	<div class="row align-items-center">
+	<div class="row mt-5 mb-4">
 
 		<div class="col-8">
 			<h1><?php the_title(); ?></h1>
 			<?php the_content(); ?>
 		</div>
 
-		<div class="col-4 py-4">
+		<div class="col-4">
 			<?php get_template_part('templates/sections/parts/locations','list'); ?>
 		</div>
 
