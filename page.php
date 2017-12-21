a26ef7fce9adf0b3b70b60583b18a28fe2fdac97
odil-io <github@odil.io>
2017-12-21 13:49:36 +0100

    Forum templates toegevoegd

--- page.php
+++ page.php
@@ -1,3 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+
+	<?php get_template_part('templates/content', get_post_type()); ?>
+
 <?php endwhile; ?>
