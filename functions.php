e16df6064b13905cb389aefdd8b28ea2008f0722
odil-io <github@odil.io>
2017-12-21 12:55:19 +0100

    Core: plugins afhandeling verplaatst
    Dit ten goede voor de leesbaarheid en structuur.

--- functions.php
+++ functions.php
@@ -1,20 +1,11 @@
 <?php
-/**
- * Sage includes
- *
- * The $sage_includes array determines the code library included in your theme.
- * Add or remove files to the array as needed. Supports child theme overrides.
- *
- * Please note that missing files will produce a fatal error.
- *
- * @link https://github.com/roots/sage/pull/1042
- */
 $sage_includes = [
 	'vendor/autoload.php',     // Autoload
 	'lib/assets.php',          // Scripts and stylesheets
 	'lib/extras.php',          // Custom functions
 	'lib/posts.php',           // Custom post types
 	'lib/setup.php',           // Theme setup
+	'lib/plugins.php',         // Theme plugins
 	'lib/titles.php',          // Page titles
 	'lib/wrapper.php',         // Theme wrapper class
 	'lib/customizer.php',      // Theme customizer
--- lib/setup.php
+++ lib/setup.php
@@ -117,60 +117,6 @@ function assets() {
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
-/**
- * Theme Plugins
- */
-function register_required_plugins() {
-
-    $plugins = array(
-        array(
-            'name'               => 'Advanced Custom Fields Pro',
-            'slug'               => 'advanced-custom-fields-pro',
-            'source'             => '/advanced-custom-fields-pro.zip',
-            'required'           => true,
-            'force_activation'   => true,
-            'force_deactivation' => true,
-        ),
-        array(
-            'name'               => 'Gravity Forms',
-            'slug'               => 'gravityforms',
-            'source'             => '/gravityforms.zip',
-            'required'           => true,
-            'force_activation'   => true,
-            'force_deactivation' => true,
-        ),
-				array(
-            'name'               => 'Advanced Custom Fields: Gravityforms Add-on',
-            'slug'               => 'acf-gravityforms-add-on',
-            'required'           => true,
-            'force_activation'   => true,
-            'force_deactivation' => true,
-        ),
-				array(
-            'name'               => 'Advanced Custom Fields: Post2Post Add-on',
-            'slug'               => 'post-2-post-for-acf',
-            'required'           => true,
-            'force_activation'   => true,
-            'force_deactivation' => true,
-        ),
-    );
-
-    $config = array(
-        'id'           => 'sage',
-        'default_path' => get_template_directory() . '/lib/plugins',
-        'menu'         => 'tgmpa-install-plugins',
-        'parent_slug'  => 'themes.php',
-        'capability'   => 'edit_theme_options',
-        'has_notices'  => true,
-        'dismissable'  => true,
-        'is_automatic' => true,
-    );
-
-    tgmpa( $plugins, $config );
-}
-add_action( 'after_setup_theme', __NAMESPACE__ . '\\register_required_plugins' );
-
-
 /*
  * ACF Google Maps API Key
  */
