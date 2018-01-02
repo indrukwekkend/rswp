56ff36cf733a68759a60bdf71bf4d2a7b1285859
odilio <github@odil.io>
2018-01-02 10:47:52 +0100

    Fix: conditioneren velden

--- templates/sections/parts/locations-address.php
+++ templates/sections/parts/locations-address.php
@@ -5,16 +5,16 @@ $address = get_field('street') . " " . get_field('housenumber');
 $postal = get_field('postalcode') ." ". get_field('location');
 $email = get_field('email');
 ?>
-<?php if($name && $phone && $address && $postal && $email): ?>
-	<section class="address">
-		<h1>RSWP <?php the_title(); ?></h1>
-		<div><?= $name; ?></div>
-		<div>Tel.: <?= $phone; ?></div>
-		<div><?= $address; ?></div>
-		<div><?= $postal; ?></div>
-		<div><?= $email; ?></div>
-		<?php if(get_field('forum_url')): ?>
-				<a class="btn btn-primary mt-3 text-script" href="<?php the_field('forum_url'); ?>"><i>RSWP <? the_title(); ?> Forum</i></a>
-		<?php endif; ?>
-	</section>
-<?php endif; ?>
+
+<section class="address">
+	<h1>RSWP <?php the_title(); ?></h1>
+	<?=($name) ? '<div>'.$name.'</div>' : ''; ?>
+	<?=($phone) ? '<div>Tel.:'.$phone.'</div>' : ''; ?>
+	<?=($address) ? '<div>'.$address.'</div>' : ''; ?>
+	<?=($postal) ? '<div>'.$postal.'</div>' : ''; ?>
+	<?=($email) ? '<div>'.$email.'</div>' : ''; ?>
+	<?php if(get_field('forum_url')): ?>
+			<a class="btn btn-primary mt-3 text-script" href="<?php the_field('forum_url'); ?>"><i>RSWP <? the_title(); ?> Forum</i></a>
+	<?php endif; ?>
+	<div class="mb-3"></div>
+</section>
